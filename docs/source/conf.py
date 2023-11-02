5228021ebfedde3f768a06adf9f3f4a388351d0a
李成刚 <lichenggang@uniontech.com>
2023-11-02 16:36:40 +0800

    feat: update cppy to 1.2.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,7 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
-
+from cppy import __version__
 
 # -- Project information -----------------------------------------------------
 
@@ -23,10 +23,8 @@ project = 'cppy'
 copyright = '2019, Nucleic team'
 author = 'Nucleic team'
 
-# The short X.Y version
-version = ''
 # The full version, including alpha/beta/rc tags
-release = '1.1.0'
+release = __version__
 
 
 # -- General configuration ---------------------------------------------------
@@ -88,7 +86,7 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# html_static_path = ['_static']
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
